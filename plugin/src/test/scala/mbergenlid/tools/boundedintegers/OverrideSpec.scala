b2c9c06099ba0ddcfd5ad2420cc7dbc9436b6857
Marcus Bergenlid <marcus.bergenlid@gmail.com>
2014-06-13 11:00:00 +1000

    Added test for enforcing constraints of overridden symbol.

--- plugin/src/test/scala/mbergenlid/tools/boundedintegers/OverrideSpec.scala
+++ plugin/src/test/scala/mbergenlid/tools/boundedintegers/OverrideSpec.scala
@@ -3,7 +3,6 @@ package mbergenlid.tools.boundedintegers
 class OverrideSpec extends PluginTestRunner {
 
     test("Overridden field should inherit constraints") {
-
         compile(
             """
             |trait Super {
@@ -23,6 +22,22 @@ class OverrideSpec extends PluginTestRunner {
             |val y = s.value //Compile time type is now Super
             |true
         """.stripMargin)(Nil)
+    }
+
+    test("Overridden field should enforce constraints") {
+        compile(
+            """
+            |trait Super {
+            |  @GreaterThan(0)
+            |  def value: Int
+            |}
+            |
+            |class Concrete extends Super {
+            |  def value = -1
+            |}
+            |
+            |true
+        """.stripMargin)(List(8))
     }
 
 }
