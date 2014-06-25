356f1fc9d5734bb1f3640a14b898c0092cad8a94
Marcus Bergenlid <marcus.bergenlid@gmail.com>
2014-06-25 21:33:23 +1000

    Modified .gitignore file.
    Added project directory which was previously ignored.

---
+++ project/Build.scala
@@ -0,0 +1,38 @@
+import sbt._
+import Keys._
+import sbt.Classpaths._
+
+object PluginBuild extends Build {
+  lazy val root = Project(id = "invariants",
+                          base = file("."),
+                          settings = testSettings) aggregate(annotations, plugin, test)
+
+  lazy val annotations = Project(id = "annotations",
+                         base = file("annotations"))
+
+  lazy val plugin = Project(id = "plugin",
+                         base = file("plugin"))
+
+  lazy val test = Project(id = "test",
+                          base = file("test"))
+
+  lazy val extractJarsTarget = SettingKey[File]("extract-jars-target", "Target directory for extracted JAR files")
+
+  lazy val extractJars = TaskKey[Unit]("extract-jars", "Extracts JAR files")
+
+  lazy val testSettings = Defaults.defaultSettings ++ Seq(
+
+    // define the target directory
+    extractJarsTarget <<= (target)(_ / "scala-2.10/classes"),
+
+    // task to extract jar files
+    extractJars <<= (packageBin in Compile in annotations, packageBin in Compile in plugin, extractJarsTarget) map { (bin1, bin2, dir) =>
+      println("Running extract jars")
+      println(s"${bin1.absolutePath} ${bin2.absolutePath}")
+      IO.unzip(bin1, dir, {n: String => !n.startsWith("META-INF")})
+      IO.unzip(bin2, dir, {n: String => !n.startsWith("META-INF")})
+    },
+
+    packageBin in Compile <<= (packageBin in Compile) dependsOn extractJars
+  )
+}
