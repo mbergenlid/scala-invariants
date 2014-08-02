8a92fdd587c9b69d953cd604f4cbd43800f03f16
Marcus Bergenlid <marcus.bergenlid@gmail.com>
2014-08-02 14:35:35 +1000

    Moved BoundToInput to test module.

---
+++ test/src/main/resources/compiletests/BoundToInput.scala
@@ -0,0 +1,38 @@
+package compiletests
+
+import mbergenlid.tools.boundedintegers.annotations.{GreaterThan, Equal}
+
+object BoundToInput {
+
+  def boundToSimpleInput() = {
+    @Equal("n")
+    def myMethod1(n: Int) = n
+
+    @Equal("n")
+    def myMethod2(n: Int) = n + 1 //error
+  }
+
+  def useInputBoundMethod() = {
+    @Equal("n")
+    def myMethod(n: Int) = n
+
+    @Equal(10)
+    val x = myMethod(10)
+    x
+  }
+
+  def boundToDoubleInput() = {
+    @Equal("n")
+    def myMethod1(n: Double) = n
+
+    @Equal("n")
+    def myMethod2(n: Double) = n + 1 //error
+  }
+
+  def boundInputToOtherInput() = {
+    def myMethod(from: Int, @GreaterThan("from") to: Int) = to - from
+
+    myMethod(1,2)
+    myMethod(2,1) //error
+  }
+}
