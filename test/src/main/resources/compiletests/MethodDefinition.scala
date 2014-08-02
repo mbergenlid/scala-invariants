2ce99d9d2a5b83e5995c702647b17be94611ed50
Marcus Bergenlid <marcus.bergenlid@gmail.com>
2014-08-02 10:07:45 +1000

    Moved MethodDefinitionSpec to test module.

---
+++ test/src/main/resources/compiletests/MethodDefinition.scala
@@ -0,0 +1,20 @@
+package compiletests
+
+import mbergenlid.tools.boundedintegers.annotations.{Equal, LessThanOrEqual}
+
+object MethodDefinition {
+  @LessThanOrEqual(10)
+  def invalidMethod1 = 11 //error
+
+  @LessThanOrEqual(10)
+  def validMethod1 = 5
+
+  @Equal(11)
+  val x = 11
+
+  @LessThanOrEqual(10)
+  def invalidMethod2 = x //error
+
+  @LessThanOrEqual(10)
+  def validMethod2 = x - 1
+}
